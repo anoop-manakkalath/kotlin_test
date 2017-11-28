6e61a678e12ca0c5aaf1010be69132698bfb2b82
Anoop Manakkalath <anoop.manakkalath@gmail.com>
2017-11-28 09:23:40 +0530

    Updated kotlin version and file reading styles

--- src/main/kotlin/com/kotlin/basic/test/fileOps.kt
+++ src/main/kotlin/com/kotlin/basic/test/fileOps.kt
@@ -3,8 +3,7 @@ package com.kotlin.basic.test
 import java.io.*
 import java.nio.ByteBuffer
 import java.nio.charset.Charset
-import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.charset.StandardCharsets
 
 fun main(args: Array<String>) {
     val x1 = System.nanoTime()
@@ -12,7 +11,7 @@ fun main(args: Array<String>) {
     val x2 = System.nanoTime()
     getFileNIO("src/main/resources/customer.xml")
     val x3 = System.nanoTime()
-    getFileNew("src/main/resources/customer.xml");
+    getFileDirect("src/main/resources/customer.xml");
     val x4 = System.nanoTime()
     println("${x2-x1}")
     println("${x3-x2}")
@@ -21,39 +20,34 @@ fun main(args: Array<String>) {
 
 fun getFileIO(file: String): String {
     val content = StringBuilder()
-    val file = File(file)
-    val fr = FileReader(file)
-    val br = BufferedReader(fr)
-    var textRead = br.readLine()
-    while (textRead != null) {
-        content.append(textRead).append("\n")
-        textRead = br.readLine()
-    }
-    br.close();
+    val newLine = "\n"
+    val inputStream: InputStream = File(file).inputStream()
+    inputStream.bufferedReader().useLines { lines -> lines.forEach { content.append(it).append(newLine) } }
+    println(content)
+    return content.toString()
+}
+
+fun getFileDirect(file: String): String {
+    val content = StringBuilder()
+    val newLine = "\n"
+    File(file).useLines { lines -> lines.forEach { content.append(it).append(newLine) }}
     println(content)
     return content.toString()
 }
 
 fun getFileNIO(file: String): String {
     val content = StringBuilder()
-    val aFile = RandomAccessFile(file, "r")
+    val charSet = Charset.forName(StandardCharsets.UTF_8.name())
+    val aFile = RandomAccessFile (file, "r")
     val fChannel = aFile.channel
-    val buffer = ByteBuffer.allocate(1024)
-    while (fChannel.read(buffer) > 0) {
+    val buffer = ByteBuffer.allocate(2048)
+    while (fChannel.read(buffer) != -1) {
         buffer.flip()
-        content.append(Charset.forName("utf-8").decode(buffer).toString())
+        content.append(charSet.decode(buffer))
         buffer.flip()
     }
     fChannel.close()
     aFile.close()
-    println(content)
-    return content.toString()
-}
-
-fun getFileNew(file: String): String {
-    val content = StringBuilder()
-    val paths = Paths.get(file)
-    Files.lines(paths).forEachOrdered { line ->  content.append(line).append("\n")}
     println(content)
     return content.toString()
 }
